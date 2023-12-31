7a2b8f015536dfe942ab58127ba515e652bc5c10
NguyenHoangKhas <khapromh321@gmail.com>
2023-12-31 12:11:14 +0700

    model student

--- Models/Sudent.cs
+++ Models/Sudent.cs
@@ -7,5 +7,11 @@ namespace LTW_Buoi2.Models
 {
     public class Sudent
     {
+        public int ID { get; set; }
+        public String name { get; set; }
+        public DateTime Birhday { get; set; }
+        public int addressID { get; set; }
+        public String addressFull { get; set; }
+
     }
 }
