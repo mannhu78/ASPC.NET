5db1246d8727e1784ae73ae3d683863d0cb671b4
NguyenHoangKhas <khapromh321@gmail.com>
2023-12-31 11:34:34 +0700

    first commit

---
+++ Models/Sudent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LTW_Buoi2.Models
+{
+    public class Sudent
+    {
+    }
+}
---
+++ Views/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LTW_Buoi2.Views
+{
+    public class Student
+    {
+    }
+}
